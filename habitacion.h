f88bee8ce827d33822a998e2bb386219359962e6
UnaiiGonzalezz <116153554+UnaiiGonzalezz@users.noreply.github.com>
2024-05-20 20:03:46 +0200

    habitacion.h

---
+++ habitacion.h
@@ -0,0 +1,18 @@
+/*
+ * habitacion.h
+ *
+ *  Created on: 20 may 2024
+ *      Author: GONZA
+ */
+
+#ifndef HABITACION_H_
+#define HABITACION_H_
+
+class habitacion {
+public:
+	habitacion();
+	virtual ~habitacion();
+	habitacion(const habitacion &other);
+};
+
+#endif /* HABITACION_H_ */
