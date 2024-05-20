f1b001aabe9b653421e39be91bb3712276b5cba9
UnaiiGonzalezz <116153554+UnaiiGonzalezz@users.noreply.github.com>
2024-05-20 20:28:58 +0200

    modif. hab.h

--- habitacion.h
+++ habitacion.h
@@ -1,10 +1,3 @@
-/*
- * habitacion.h
- *
- *  Created on: 20 may 2024
- *      Author: GONZA
- */
-
 #ifndef HABITACION_H_
 #define HABITACION_H_
 
@@ -17,8 +10,20 @@ private:
 	char* estado;
 public:
 	habitacion();
+	habitacion(int id, char* tipo, int capacidad, float precio, char* estado);
 	virtual ~habitacion();
-	habitacion(const habitacion &other);
+	habitacion(const habitacion &h);
+
+	int getId();
+	char* getTipo();
+	int getCapacidad();
+	float getPrecio();
+	char* getEstado();
+	void getId(int id);
+	void getTipo(char* tipo);
+	void getCapacidad(int capacidad);
+	void getPrecio(float precio);
+	void getEstado(char* estado);
 };
 
 #endif /* HABITACION_H_ */
