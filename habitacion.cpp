7833a0070588fc1912996a5d65e15bf390056c0d
UnaiiGonzalezz <116153554+UnaiiGonzalezz@users.noreply.github.com>
2024-05-20 20:06:43 +0200

    habiacion.cpp

---
+++ habitacion.cpp
@@ -0,0 +1,23 @@
+/*
+ * habitacion.cpp
+ *
+ *  Created on: 20 may 2024
+ *      Author: GONZA
+ */
+
+#include "habitacion.h"
+
+habitacion::habitacion() {
+	// TODO Auto-generated constructor stub
+
+}
+
+habitacion::~habitacion() {
+	// TODO Auto-generated destructor stub
+}
+
+habitacion::habitacion(const habitacion &other) {
+	// TODO Auto-generated constructor stub
+
+}
+
