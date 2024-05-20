daba38921e2547c2b974d614a604d2aa43df8df6
UnaiiGonzalezz <116153554+UnaiiGonzalezz@users.noreply.github.com>
2024-05-20 20:47:51 +0200

    const hab.cpp

--- habitacion.cpp
+++ habitacion.cpp
@@ -1,23 +1,37 @@
-/*
- * habitacion.cpp
- *
- *  Created on: 20 may 2024
- *      Author: GONZA
- */
-
 #include "habitacion.h"
+#include <string.h>
+#include <iostream>
 
-habitacion::habitacion() {
-	// TODO Auto-generated constructor stub
+using namespace std;
 
+habitacion::habitacion()
+{
+	this->id = 0;
+	this->tipo = NULL;
+	this->capacidad = 0;
+	this->precio = 0;
+	this->estado = NULL;
 }
 
-habitacion::~habitacion() {
-	// TODO Auto-generated destructor stub
+habitacion::habitacion(int id, char* tipo, int capacidad, float precio, char* estado)
+{
+	this->id = id;
+	this->tipo = new char(strlen(tipo) + 1);
+	strcpy(this->tipo, tipo);
+	this->capacidad = capacidad;
+	this->precio = precio;
+	this->estado = new char(strlen(estado) + 1);
+	strcpy(this->estado, estado);
 }
 
-habitacion::habitacion(const habitacion &other) {
-	// TODO Auto-generated constructor stub
-
+habitacion::habitacion(const habitacion &h)
+{
+	this->id = h.id;
+	this->tipo = new char(strlen(h.tipo) + 1);
+	strcpy(this->tipo, h.tipo);
+	this->capacidad = h.capacidad;
+	this->precio = h.precio;
+	this->estado = new char(strlen(h.estado) + 1);
+	strcpy(this->estado, h.estado);
 }
 
